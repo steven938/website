8ae8df87dea1d6f33a58ef811536e770267d7c61
schen938 <s_chen@hotmail.ca>
2020-04-15 12:22:23 -0700

    Temp commit

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require('cors');
+const favicon = require('express-favicon');
+const port = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(favicon(__dirname + '/client/public/favicon.ico'));
 let bodyParser     =        require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/api', (req, res) => {
+app.get('/app', (req, res) => {
   res.send("backend call")
 });
 
@@ -23,5 +23,4 @@ app.get('*', (req, res) => {
   // res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const port = 5000;
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(port);
