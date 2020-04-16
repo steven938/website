0e58c55e5fab5b9edfd296e77ac4983e51d174bc
schen938 <s_chen@hotmail.ca>
2020-04-15 21:32:11 -0700

    Testing error handling in Ajax

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ app.post('/form', (req, res) => {
   console.log(req.body);
   const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
   if(!emailRegexp.test(req.body.email)){
-    res.status(422).send(req.body.email + " is an invalid email").end();
+    // set the status of the response object to 422 and then send error message
+    res.status(422).send(req.body.email + " is an invalid email! Please enter a valid email.");
   };
-  res.status(200).end();
+  res.end();
 });
 
 app.listen(port);
