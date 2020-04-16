8aa4fd47e81ab7a9b7fd1ae6caa3866c3429cabf
schen938 <s_chen@hotmail.ca>
2020-04-15 20:26:24 -0700

    Test email verification 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: false}));
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
