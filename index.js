570691dfdf805d51d420774009a65c4178984fc2
schen938 <s_chen@hotmail.ca>
2020-04-15 20:01:14 -0700

    Test email verification 2

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 8080;
 
+
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
 
 app.post('/form', (req, res) => {
+  console.log(req.body);
   const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
   if(!emailRegexp.test(req.body.contactEmail)){
-    console.log(req);
     res.status(422).send(req.body.contactEmail + " is an invalid email");
   };
 
